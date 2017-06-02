0585dd0f33a6f1b21e413f3fd5a30b3dac7149c2
CaptainCorndog <trevor.horine@gmail.com>
2017-06-02 09:27:43 -0700

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,12 +1,16 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Container;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class Board extends JPanel{
+public class Board implements ActionListener{
 	JFrame frame = new JFrame("Snake!");
 	Playarea playarea = new Playarea();
 	Container south = new Container();
@@ -21,8 +25,11 @@ public class Board extends JPanel{
 		frame.add(south, BorderLayout.SOUTH);
 		south.setLayout(new GridLayout());
 		south.add(Easy);
+		Easy.addActionListener(this);
 		south.add(Medium);
+		Medium.addActionListener(this);
 		south.add(Hard);
+		Hard.addActionListener(this);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 		frame.repaint();
@@ -32,6 +39,15 @@ public class Board extends JPanel{
 	public static void main(String[] args) {
 		new Board();
 
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource().equals(Easy)){
+			Snake snake = new Snake((int)Math.random()*400,(int)Math.random()*400);
+			playarea.addsnake(snake);
+			frame.repaint();
+		}
+		
 	}
 
 }
