b8322bbba6c745ec85cf3299c4ac983ceb9b595c
CaptainCorndog <trevor.horine@gmail.com>
2017-06-02 10:19:34 -0700

    Update Playarea.java

--- Playarea.java
+++ Playarea.java
@@ -6,22 +6,25 @@ import javax.swing.JPanel;
 
 public class Playarea extends JPanel{
 	ArrayList<Snake> snakes = new ArrayList<Snake>();
+	ArrayList<Food> foodz = new ArrayList<Food>();
 	public Playarea(){
 		super();
 	}
 	public void addsnake(Snake s){
 		snakes.add(s);
 	}
-
-	
-		
+	public void addfood(Food f){
+		foodz.add(f);
+	}
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 		g.setColor(Color.BLACK);
 		g.fillRect(0, 0, getWidth(), getHeight());
 		for (int a = 0; a < snakes.size(); a++) {
 			snakes.get(a).paintMe(g);
-		
+		}
+		for (int a = 0; a < foodz.size(); a++) {
+			foodz.get(a).paintMe(g);
 		}
 	}
 }
