9d556f9e12a7b9c1bf468555d0850c15b6fd240b
CaptainCorndog <trevor.horine@gmail.com>
2017-06-02 10:20:24 -0700

    Update Food.java

--- Food.java
+++ Food.java
@@ -1,11 +1,29 @@
+import java.awt.Color;
+import java.awt.Graphics;
 
 public class Food {
 	int x;
 	int y;
-	boolean real;
-	public Food(int newx, int newy, boolean newReal) {
+	int width = 10;
+	int height = 10;
+	public Food(int newx, int newy) {
 		x = newx;
 		y = newy;
-		real = newReal;
+	}
+	public void paintMe(Graphics g) {
+		g.setColor(Color.RED);
+		g.fillRect(x, y, width, height);
+	}
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
 	}
 }
