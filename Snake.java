a6b1e564d4f1fe7dec687f2fefee1f81b0f1d4f2
CaptainCorndog <trevor.horine@gmail.com>
2017-06-02 09:28:44 -0700

    Update Snake.java

--- Snake.java
+++ Snake.java
@@ -2,18 +2,14 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 public class Snake {
-	int x = 0;
-	int y = 0;
+	int x = 10;
+	int y = 10;
 	int width = 10;
 	int height = 10;
 	int length;
-	int xspeed;
-	int yspeed;
-	public Snake (int newx, int newy, int newxspeed, int newyspeed) {
+	public Snake (int newx, int newy) {
 		x = newx;
 		y = newy;
-		xspeed = newxspeed;
-		yspeed = newyspeed;
 	}
 	public void paintMe(Graphics g) {
 		g.setColor(Color.GREEN);
