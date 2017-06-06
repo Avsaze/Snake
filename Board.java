b2d4867bc4eeefad86226a1a9264b93b6b31c963
CaptainCorndog <trevor.horine@gmail.com>
2017-06-06 09:21:02 -0700

    Update Board.java

--- Board.java
+++ Board.java
@@ -8,23 +8,26 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class Board implements ActionListener{
+	int score = 0;
+	String Score = Integer.toString(score);
 	JFrame frame = new JFrame("Snake!");
 	Playarea playarea = new Playarea();
 	Container south = new Container();
 	JButton Easy = new JButton("Easy");
 	JButton Medium = new JButton("Medium");
 	JButton Hard = new JButton("Hard");
-	JTextField scoretf = new JTextField();
+	JLabel scorel = new JLabel("Score: " + Score);
 	final int undecided =0;
 	final int easy = 1;
 	final int med = 2;
 	final int hard = 3;
 	int dif = undecided;
-	int score = 0;
+	
 	
 	public Board(){
 		frame.setSize(1000,1000);
@@ -38,6 +41,7 @@ public class Board implements ActionListener{
 		Medium.addActionListener(this);
 		south.add(Hard);
 		Hard.addActionListener(this);
+		frame.add(scorel, BorderLayout.NORTH);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 		frame.repaint();
@@ -86,8 +90,18 @@ public class Board implements ActionListener{
 		if(playarea.foodz.isEmpty() == true){
 			Food food = new Food((int)(Math.random()* playarea.getWidth()),(int)(Math.random()* playarea.getHeight()));
 			playarea.addfood(food);
+			score = score + 3;
+			Score = Integer.toString(score);
+			scorel.setText("Score: " + Score);
 			frame.repaint();
 		}
+		if((playarea.snakes.get(0).getX() <= 0)||
+			(playarea.snakes.get(0).getX() >= playarea.getWidth())||
+			(playarea.snakes.get(0).getY() <= 0)||
+			(playarea.snakes.get(0).getY() >= playarea.getHeight())){
+			
+		}
+
 	}
 
 }
